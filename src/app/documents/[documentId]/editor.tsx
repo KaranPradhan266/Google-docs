a142d8278da4b55a498d3e27c701d2679dc504c0
KaranPradhan266 <karanpradhan266@gmail.com>
2026-01-08 08:55:08 -0800

    Added tiptap extension and their custom styling

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -2,6 +2,15 @@
 
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import TaskItem from '@tiptap/extension-task-item';
+import TaskList from '@tiptap/extension-task-list';
+import Table from '@tiptap/extension-table';
+import TableCell from '@tiptap/extension-table-cell';
+import TableRow from '@tiptap/extension-table-row';
+import TableHeader from '@tiptap/extension-table-header';
+import Image from '@tiptap/extension-image';
+import ImageResize from 'tiptap-extension-resize-image';
+
 
 const Editor = () => {
     const editor = useEditor({
@@ -11,8 +20,35 @@ const Editor = () => {
             class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
         }
     },
-    extensions: [StarterKit],
-    content: '<p>Hello World! 🌎️</p>',
+    extensions: [
+        StarterKit,
+        TaskList,
+        TaskItem.configure({
+            nested: true,
+        }),
+       Table,
+       TableCell,
+       TableRow,
+       TableHeader,
+       Image,
+       ImageResize,
+    ],
+    content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+      `,
     // Don't render immediately on the server to avoid SSR issues
     immediatelyRender: false,
   })
