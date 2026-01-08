3906558bef2462c28842ad26569c872bf687cd5f
KaranPradhan266 <karanpradhan266@gmail.com>
2026-01-08 07:53:16 -0800

    Document Folder structure

---
+++ src/app/documents/[documentId]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+interface DocumentIdPageProps {
+    params: Promise<{ documentId: string}>;
+}
+const DocumentIdPage = async ({params}: DocumentIdPageProps) => {
+    const awaitedParams = await params;
+    const documentId = awaitedParams.documentId;
+    return (
+    <div>DocumentId: {documentId} </div>
+  )
+}
+
+export default DocumentIdPage;
