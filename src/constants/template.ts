944674e0b392bae3ed8c045741b287e03fecad15
KaranPradhan266 <karanpradhan266@gmail.com>
2026-01-10 00:30:16 -0800

    Added templates

---
+++ src/constants/template.ts
@@ -0,0 +1,37 @@
+export const templates = [
+    {
+        id:"blank", 
+        label: "Blank Document", 
+        imageUrL:"/blank-document.svg"
+    },
+    {
+        id:"software-proposal", 
+        label: "Software development proposal", 
+        imageUrL:"/software-proposal.svg"
+    },
+    {
+        id:"project-proposal", 
+        label: "Project proposal", 
+        imageUrL:"/project-proposal.svg"
+    },
+    {
+        id:"business-letter", 
+        label: "Business letter", 
+        imageUrL:"/business-letter.svg"
+    },
+    {
+        id:"resume", 
+        label: "Resume", 
+        imageUrL:"/resume.svg"
+    },
+    {
+        id:"cover-letter", 
+        label: "Cover letter", 
+        imageUrL:"/cover-letter.svg"
+    },
+    {
+        id:"letter", 
+        label: "Letter", 
+        imageUrL:"/letter.svg"
+    },
+];
