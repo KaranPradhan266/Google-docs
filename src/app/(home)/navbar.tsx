764a74473a8d752e63fd5a508e2b7851ba9cf090
KaranPradhan266 <karanpradhan266@gmail.com>
2026-01-09 23:46:52 -0800

    Added search component

---
+++ src/app/(home)/navbar.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import Image from "next/image";
+import SearchInput from "./search-input";
+
+
+const navbar = () => {
+  return (
+    <nav className="flex items-center justify-between h-full w-full">
+        <div className="flex gap-3 items-center shrink-0 pr-6">
+            <Link href="/">
+            <Image src="/logo.svg" alt="logo" width={30} height={30} />
+            </Link>
+            <h3 className="text-xl">Collab RT</h3>
+        </div>
+        <SearchInput />
+        <div />
+    </nav>
+  )
+}
+
+export default navbar
